bdb72691036d8906cdae752dfa70fb19cc403cf6
Rafael Rodrigues Bento <rafael.rodrigues.bento@outlook.com>
2022-11-06 16:02:05 -0300

    Finalizada a aula 04

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
 
 import { poolRoutes } from "./routes/pool";
 import { userRoutes } from "./routes/user";
@@ -16,13 +17,18 @@ async function bootstrap(){
     origin: true
   })
 
+  await fastify.register(jwt, {
+    secret: 'nlwcopa2022',
+  })
+
+
   await fastify.register(poolRoutes);
   await fastify.register(userRoutes);
   await fastify.register(guessRoutes);
   await fastify.register(gameRoutes);
   await fastify.register(authRoutes);
 
-  await fastify.listen({ port: 3333, /*host: "0.0.0.0"*/})
+  await fastify.listen({ port: 3333, host: "0.0.0.0"})
 
 }
 
